0c7214001a6ae60ba0e66f793c1fe7d9b3f2ee2f
Nanda Kumar Reddy Gollapalli <115801299+nandakumarreddy04@users.noreply.github.com>
2024-01-29 18:39:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) 
 
@@ -6,5 +6,8 @@ app = Flask(__name__)
 @app.route("/") 
 def helloworld():
 	return "Hello World, from Nanda-1"
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 
